df074b0ae9ed6a2f0b32b53dfb77d7c7340ef8f7
Alexander Matrunich <alexander@matrunich.com>
2015-08-04 12:21:39 +0200

    Cached version of getComtradeM49()

--- OrangeBook/tradeR/getComtradeM49.R
+++ OrangeBook/tradeR/getComtradeM49.R
@@ -1,8 +1,17 @@
 #' Retrieve modified area codes used in UN Comtrade
 #' 
-#' These codes are used in reporter column of Tariffline
+#' These codes are used in reporter column of Tariffline. This function has 
+#' side-effect: by default it creates cached copy of data in invisible variable
+#' .ComtradeM49areas
+#' 
+#' @param cache Logical. Should cache version be used if it exists or be created
+#'   if does not exist. TRUE by default.
 
-getComtradeM49 <- function() {
+
+getComtradeM49 <- function(cache = T) {
+  
+  if(exists(".ComtradeM49areas") & cache)
+    return(.ComtradeM49areas)
   
   url <- "http://comtrade.un.org/data/cache/reporterAreas.json"
   
@@ -10,6 +19,15 @@ getComtradeM49 <- function() {
   
   colnames(tbl) <- c("code", "name")
   
-  tbl[tbl$code != "all",]
+  tbl <- tbl[tbl$code != "all",]
+  
+  if(cache) {
+    .ComtradeM49areas <<- tbl
+    return(.ComtradeM49areas)
+  }
+  
+  if(!cache) return(tbl)
+  
+  stop("Unxpected logic in getComtradeM49()")
   
 }
