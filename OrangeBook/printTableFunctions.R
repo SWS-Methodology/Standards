008a268d21a7b6d70da8e77fe0d245be623eee2b
rockclimber112358 <rockclimber112358@gmail.com>
2015-08-17 16:51:33 +0200

    Updates to wheat example

--- OrangeBook/printTableFunctions.R
+++ OrangeBook/printTableFunctions.R
@@ -1,14 +1,15 @@
 ## Rounding numbers for display
 roundNum = function(x){
+    if(is.na(x)){
+        return(x)
+    }
     initialSign = sign(x)
     x = abs(x)
     # 1 or 2 digits: multiple of 5.
     # 3 digits: multiple of 10.
     # 4 to 7 digits: multiple of 100
     # 8+ digits: 4 significant digits.
-    if(is.na(x)){
-        return(x)
-    } else if(x < 100){
+    if(x < 100){
         x = round(x/5, 0)*5
     } else if(x < 1000){
         x = round(x/10)*10
@@ -19,7 +20,7 @@ roundNum = function(x){
         x = as.numeric(x)
     }
     x = x * initialSign
-    x = formatC(x, big.mark = ",", format = "s")
+    x = prettyNum(x, big.mark = ",", scientific = FALSE)
     return(x)
 }
 
@@ -32,7 +33,6 @@ printTable = function(data, standParams, workingDir){
     printDT = printDT[, c(standParams$mergeKey, "element", "Value", "updateFlag"),
                       with = FALSE]
     printDT[, element := paste0("Value_measuredElement_", element)]
-    printDT[, Value := sapply(Value, roundNum)]
     
     ## Don't print some elements
     skippedElements = c(24110, 2413, 23210.05, 24490.92)
@@ -46,7 +46,7 @@ printTable = function(data, standParams, workingDir){
                     standParams$touristCode)
 
     ## Add bold style to updated values, round to no decimals, NA to "-"
-    printDT[, Value := as.character(round(as.numeric(Value), 0))]
+    printDT[, Value := sapply(Value, roundNum)]
     printDT[is.na(Value), Value := "-"]
     if("updateFlag" %in% colnames(printDT)){
         printDT[(updateFlag), Value := paste0("**", Value, "**")]
@@ -65,7 +65,7 @@ printTable = function(data, standParams, workingDir){
     printDT = merge(printDT, description, by = "Item")
 
     items = c("Name", "Production", "Imports", "Exports", "StockChange",
-              "Food", "Food Processing", "Feed", "Seed", "Tourist",
+              "Food", "Feed", "Seed", "Tourist",
               "Industrial", "Loss")
     sapply(items, function(colName){
         if(!colName %in% colnames(printDT)){
@@ -83,10 +83,12 @@ printProductionTable = function(data, standParams, workingDir){
     printDT = copy(data)
     printDT = printDT[, c(standParams$mergeKey, "element", "Value"),
                       with = FALSE]
-    ## Round to 0 or 4 (yield only) decimals
-    printDT[element == standParams$yieldCode, Value := round(Value, 4)]
-    printDT[element != standParams$yieldCode, Value := sapply(Value, roundNum)]
+    ## Round to the appropriate number of decimals
+    printDT[, Value := ifelse(element != standParams$yieldCode,
+                              sapply(Value, roundNum),
+                              as.character(round(Value, 4)))]
     printDT[, element := paste0("Value_measuredElement_", element)]
+    printDT[is.na(Value), Value := "-"]
     printDT = tidyr::spread(data = printDT, key = "element", value = "Value")
     
     ## Bring in item names
@@ -95,17 +97,18 @@ printProductionTable = function(data, standParams, workingDir){
     setnames(printDT, paste0("Value_measuredElement_", c(standParams$areaHarvCode,
                                                          standParams$productionCode,
                                                          standParams$yieldCode)),
-             c("Area Harvested (hectare)", "Production (tonnes)", "Yield (tonnes/hectare"))
+             c("Area Harvested (hectare)", "Production (tonnes)", "Yield (tonnes/hectare)"))
     setnames(printDT, "measuredItemCPC", "Item")
     printDT = merge(printDT, description, by = "Item")
 
-    items = c("Name", "Area Harvested", "Yield", "Production")
+    items = c("Name", "Area Harvested (hectare)", "Yield (tonnes/hectare)", "Production (tonnes)")
     sapply(items, function(colName){
         if(!colName %in% colnames(printDT)){
             printDT[, c(colName) := 0]
         }
     })
-    out = knitr::kable(printDT[, items, with = FALSE])
+    out = knitr::kable(printDT[, items, with = FALSE],
+                       align = c("l", "r", "r", "r"))
     return(out)
 }
 
@@ -123,11 +126,10 @@ printDistributionTable = function(data, standParams){
     printMean = printMean[!is.na(get(standParams$itemVar)), ]
     
     printSd = copy(data)
-    printSd[, Value := sapply(Value, roundNum)]
+    printSd[, standardDeviation := sapply(standardDeviation, roundNum)]
     printSd = printSd[, c(standParams$mergeKey, "element", "standardDeviation"),
                       with = FALSE]
     printSd[, element := paste0("Value_measuredElement_", element)]
-    printSd[, standardDeviation := round(standardDeviation)]
     printSd = tidyr::spread(data = printSd, key = "element", value = "standardDeviation")
     ## On some occassions, a strange error happens here which introduces a row
     ## of NA's.  Just delete that row:
@@ -140,24 +142,24 @@ printDistributionTable = function(data, standParams){
                     standParams$seedCode, standParams$wasteCode,
                     standParams$foodCode, standParams$stockCode,
                     standParams$importCode, standParams$exportCode,
-                    standParams$foodProcCode, standParams$industrialCode,
-                    standParams$touristCode)
+                    standParams$industrialCode, standParams$touristCode)
     
     setnames(printDT, paste0("Value_measuredElement_", fbsElements),
              c("Production", "Feed", "Seed", "Waste", "Food",
-               "StockChange", "Imports", "Exports", "Food Processing",
+               "StockChange", "Imports", "Exports",
                "Industrial", "Tourist"))
     setnames(printDT, "measuredItemCPC", "Item")
 
     items = c("Variable", "Production", "Imports", "Exports", "StockChange",
-              "Food", "Food Processing", "Feed", "Waste", "Seed", "Industrial",
+              "Food", "Feed", "Waste", "Seed", "Industrial",
               "Tourist")
     sapply(items, function(colName){
         if(!colName %in% colnames(printDT)){
             printDT[, c(colName) := 0]
         }
     })
-    out = knitr::kable(printDT[, items, with = FALSE])
+    out = knitr::kable(printDT[, items, with = FALSE],
+                       align = c("l", rep("r", ncol(printDT)-1)))
     
     return(out)
 }
@@ -183,17 +185,17 @@ printStandardizationTable = function(data, standParams, workingDir){
                    "Wheat Equivalent", "SD(Wheat Equivalent)", "Adjustment"))
         
         return(knitr::kable(printDT[, c("Name", "Production (processed)",
-                                        "SD(Production)", "Wheat Equivalent",
-                                        "SD(Wheat Equivalent)", "Adjustment"),
-                                    with = FALSE], digits = 0))
+                                        "Wheat Equivalent", "Adjustment"),
+                                    with = FALSE], digits = 0,
+                            align = c("l", "r", "r", "r")))
     } else {
         setnames(printDT, c("prodMean", "prodSd", "wheatMean", "wheatSd"),
                  c("Production (processed)", "SD(Production)",
                    "Wheat Equivalent", "SD(Wheat Equivalent)"))
         
         return(knitr::kable(printDT[, c("Name", "Production (processed)",
-                                        "SD(Production)", "Wheat Equivalent",
-                                        "SD(Wheat Equivalent)"),
-                                    with = FALSE], digits = 0))
+                                        "Wheat Equivalent"),
+                                    with = FALSE], digits = 0,
+                            align = c("l", "r", "r")))
     }
 }
